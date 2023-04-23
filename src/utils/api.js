a5bbeb295770357499b9012f2f26708360025bb8
Ozan Sevkin <sevkinozan@gmail.com>
2023-04-23 22:31:09 +1000

    fix: change baseUrl to make mock server run online

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  "https://my-json-server.typicode.com/ozansevkin/se_project_react";
 
 const headers = new Headers({
   "Content-Type": "application/json",
