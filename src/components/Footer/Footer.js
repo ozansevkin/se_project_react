fb9f63334c1eae1aab2d0a49f8a5ecd4e8c89fc8
Ozan Sevkin <sevkinozan@gmail.com>
2023-04-24 21:43:40 +1000

    feat: add current copyright year dynamically to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import "./Footer.css";
 
+const currentYear = new Date().getFullYear();
+
 function Footer() {
   return (
     <footer className="footer">
       <p className="footer__author">Developed by Ozan Sevkin</p>
-      <p className="footer__year">&copy;2023</p>
+      <p className="footer__year">&copy;{currentYear}</p>
     </footer>
   );
 }
