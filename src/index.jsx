070f331cefb47e937d742451a243b7f8ed4067c9
Ozan Sevkin <sevkinozan@gmail.com>
2023-06-27 21:59:00 +1000

    refactor: change HashRouter to BrowserRouter as the app is now live on cloud server

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "normalize.css";
 import "./index.css";
 import App from "./components/App/App";
@@ -9,9 +9,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
